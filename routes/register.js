d9f29c787bdf46486d7dbeb91bb798cb3a198cec
Udit Namdev <unamdev0@gmail.com>
2019-03-09 22:44:43 +0530

    Removed bug of overflow

--- routes/register.js
+++ routes/register.js
@@ -11,20 +11,27 @@ router.get('/',function (req,res) {
 router.post('/',function (req,res) {
     var username=req.body.username;
     var password=req.body.password;
-    User.register(new User({username:username}) ,password,function (err,user) {
-        if(err){
-            req.flash("error",err.message);
-            console.log(err);
-            res.render('register');
-        }
-        else{
-            req.flash("success","Successfully signed in");
-            passport.authenticate('local')(req,res,function () {
-                res.redirect('/campground');
+
+
+
+
+            User.register(new User({username:username}) ,password,function (err,user) {
+                if(err){
+                    req.flash("error",err.message);
+                    console.log(err);
+                    res.render('register');
+                }
+                else{
+                    req.flash("success","Successfully signed in");
+                    passport.authenticate('local')(req,res,function () {
+                        res.redirect('/campground');
+                    });
+                }
             });
-        }
-    });
-});
+        });
+
+
+
 
 
 
